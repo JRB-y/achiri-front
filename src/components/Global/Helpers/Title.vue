eb17a424b8d2d4140c4c613126fdb1cb22953bc2
jrb <jrb.youssef@gmail.com>
2020-02-15 03:57:15 +0100

    dev: create title with sub component

---
+++ src/components/Global/Helpers/Title.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="title is-4 is-capitalized has-text-weight-light is-uppercase">{{ title}}</h1>
+    <h2 class="subtitle is-6">{{subtitle}}</h2>
+    <div class="m-5"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "TITLE"
+    },
+    subtitle: {
+      type: String,
+      required: true,
+      default: "SUBTITLE"
+    }
+  }
+};
+</script>
