d5722439c8ecbf6428f637d0af399d8beb0c2a95
jrb <jrb.youssef@gmail.com>
2020-02-16 10:57:41 +0100

    Add auth store to store index

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,10 @@ import Vuex from "vuex"
 import drawer from './Global/Drawer'
 import util from './Global/Util';
 
+import auth from './auth';
+
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-  modules: { drawer, util }
+  modules: { drawer, util, auth }
 });
