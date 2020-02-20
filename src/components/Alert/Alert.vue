cdb6ad7219cbbabec1ceb36874378def8e2bbd26
jrb <jrb.youssef@gmail.com>
2020-02-20 23:16:27 +0100

    Create Alert Component

---
+++ src/components/Alert/Alert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="notification is-light"
+    :class="{'is-danger': type === 'danger', 'is-success': type === 'success'}"
+  >
+    <button class="delete"></button>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Alert",
+  props: {
+    type: {
+      type: String,
+      default: "success"
+    }
+  }
+};
+</script>
+
+<style>
+</style>
